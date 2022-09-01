7c8366e858ef7ec63aebb0aef13820bd5e71df34
Grace Kostanich <fl5812597@gmail.com>
2022-09-01 10:29:27 -0700

    Add passing test | Should decrease Skeletons health by 2
    Co-authored-by: Donovan Weber <DonovanWeber@users.noreply.github.com>
    Co-authored-by: Zachary Waggoner <CyndaZ42@users.noreply.github.com>

--- __tests__/bone.test.js
+++ __tests__/bone.test.js
@@ -1,4 +1,4 @@
-import {storeState, changeState, attackEnemy, dogState, skeletonAttack } from '../src/js/bone.js';
+import {storeState, changeState, attackEnemy, dogState, skeletonAttack, skeletonState, dogHitSkeleton } from '../src/js/bone.js';
 
 describe ('attackEnemy', () => {
 
@@ -9,7 +9,15 @@ describe ('attackEnemy', () => {
   test('Should decrease Dogs health by 2', () => {
     dogState(skeletonAttack);
     const result = dogState();
-    expect(result).toEqual({Health: -3, Strength: 2});
+    expect(result).toEqual({Health: -1, Strength: 2});
+  });
+});
+
+describe ('attackSkeleton', () => {
+  test('Should decrease Skeletons health by 2', () => {
+    skeletonState(dogHitSkeleton);
+    const result = skeletonState();
+    expect(result).toEqual({Health: -1, Strength: 2});
   });
 });
 
